84081ad3c5b348a17ed3574137e03311e5553b2e
AleksandrKompanetc <moneymacker7@gmail.com>
2024-12-02 22:23:45 +0200

    adding simple todo app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    if (input.trim() === '') return;
+    setTodos([...todos, input]);
+    setInput('');
+  }
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos); 
+  }
+
   return (
     <div className="App">
       <h1>Task List</h1>
       <input 
         type="text"
-        
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
         placeholder='Add new task...'
       />
-      <button>Add</button>
-      <ul></ul>
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => removeTodo(index)}>X</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
