c434e111b1a95b96f68d8af5f4652275af95b623
AleksandrKompanetc <moneymacker7@gmail.com>
2024-12-06 23:26:30 +0200

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
+import './App.css'
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([])
+  const [input, setInput] = useState('')
 
   const addTodo = () => {
     if (input.trim() === '') return
